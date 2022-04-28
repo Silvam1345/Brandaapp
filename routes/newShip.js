3c78cc13d7d25fcb646c1f251bd4d454e5e1ecd9
Mateus <Mateus@MacBook-Air.local>
2022-04-28 01:10:58 -0400

    added new routes for ship

--- routes/newShip.js
+++ routes/newShip.js
@@ -37,4 +37,59 @@ router.post("/newShip/", function(req, res){
   });
 });
 
+router.get("/getShip/name", function(req, res){
+  Ship.findOne({name: req.body.name}, function(error,doc){
+    if(error){
+      console.error("Error finding ship",error);
+      res.status(500).send(error);
+    }
+    else if(!doc){
+      console.log("Error: Could not find ship named "+doc);
+      res.sendStatus(404);
+    }
+    else{
+      res.send(doc);
+    }
+  });
+});  
+
+router.get("/getShip/secondaryBattery", function(req, res){
+  Ship.find({secondaryBattery: req.body.name}, function(error, docs){
+    if(error){
+      console.error("Error finding ship",error);
+      res.status(500).send(error);
+    }
+    else if(!docs){
+      console.log("Error: Could not find any ship with such secondary battery");
+      res.sendStatus(404);
+    }
+    else{
+      res.send(docs);
+    }
+  });
+
+});
+
+router.post("/updateShip/", function(req,res){
+  const filter = req.body.name;
+  const update = req.body;
+  const new_doc = Ship.findOneAndUpdate(filter, update, function(err,old_doc) {
+    if(err){
+      console.error("Error updating ship",err);
+      res.status(500).send(err);
+    }
+    else if(filter==null){
+      console.log("Error, no ship name given");
+      res.sendStatus(400);
+    }
+    else if(!old_doc){
+      console.log("Error: Could not find such ship");
+      res.sendStatus(404);
+    }
+    else{
+      res.send(new_doc);
+    }
+  });
+});
+
 module.exports = router;
